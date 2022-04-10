da4cabc0c03bbe68e474ac827b0c29769d1e78b0
streamrp75 <steamrp75@gmail.com>
2022-04-11 02:05:30 +0300

    feat: rework initialization and add config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'reflect-metadata';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.get(ConfigService);
+  process.env.SECRET_KEY = config.get<string>('SECRET_KEY');
+  const port = config.get<number>('API_PORT');
+  await app.listen(port || 3000, () => {
+    console.log('Server is running on port ' + port);
+  });
 }
 bootstrap();
