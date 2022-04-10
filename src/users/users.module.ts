bc95c4f2ead60a153b4688aee2b897f87fe1a3bb
streamrp75 <steamrp75@gmail.com>
2022-04-11 02:10:39 +0300

    init: create user module

---
+++ src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { UserService } from './services/user/user.service';
+import { UserResolver } from './resolvers/user/user.resolver';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY || 'SECRET_KEY',
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
+  ],
+  providers: [UserService, UserResolver],
+})
+export class UsersModule {}
