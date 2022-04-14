e7561643f47d469b0318585254f3c71850af32c5
streamrp75 <steamrp75@gmail.com>
2022-04-14 22:14:51 +0300

    feat: connect another modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,27 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { UserService } from './services/user/user.service';
-import { UserResolver } from './resolvers/user/user.resolver';
+import { UsersService } from './services/user/users.service';
 import { JwtModule } from '@nestjs/jwt';
-
+import { AuthModule } from '../auth/auth.module';
+import { UsersController } from './controllers/users.controller';
+import { UserVideoEntity } from '../videos/entities/user-video.entity';
+import { VideoEntity } from '../videos/entities/video.entity';
+import { FilesModule } from '../files/files.module';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity, UserVideoEntity, VideoEntity]),
+    forwardRef(() => AuthModule),
     JwtModule.register({
       secret: process.env.SECRET_KEY || 'SECRET_KEY',
       signOptions: {
         expiresIn: '24h',
       },
     }),
+    FilesModule,
   ],
-  providers: [UserService, UserResolver],
+  providers: [UsersService],
+  exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
