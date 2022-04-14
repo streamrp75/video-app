8d3e78b20b29d710af345eda0dbab99ff3d0d53c
streamrp75 <steamrp75@gmail.com>
2022-04-14 22:27:42 +0300

    feat: change registration input

--- src/users/inputs/create-user.input.ts
+++ src/users/inputs/create-user.input.ts
@@ -1,13 +1,5 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
 export class CreateUserInput {
-  @Field()
   email: string;
-
-  @Field()
   username: string;
-
-  @Field()
   password: string;
 }
