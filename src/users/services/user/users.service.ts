3a98c1e491d5db49b1c3d729e083a0d283295589
streamrp75 <steamrp75@gmail.com>
2022-04-14 22:13:37 +0300

    feat: create video service

---
+++ src/users/services/user/users.service.ts
@@ -0,0 +1,115 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../../entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from '../../inputs/create-user.input';
+import { LoginUserInput } from '../../inputs/login-user.input';
+import { VideoEntity } from '../../../videos/entities/video.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UserVideoEntity } from '../../../videos/entities/user-video.entity';
+import { CreateVideoInput } from '../../../videos/inputs/create-video.input';
+import { FilesService } from '../../../files/services/files.service';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
+    @InjectRepository(UserVideoEntity)
+    private userVideoRepository: Repository<UserVideoEntity>,
+    @InjectRepository(VideoEntity)
+    private videoRepository: Repository<VideoEntity>,
+    private filesService: FilesService,
+  ) {}
+
+  async createUser(userInput: CreateUserInput): Promise<object> {
+    return this.userRepository.save(userInput);
+  }
+
+  async getUserByEmail(loginUserInput: LoginUserInput): Promise<UserEntity> {
+    return await this.userRepository.findOne({
+      email: loginUserInput.email,
+    });
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return await this.userRepository.find();
+  }
+
+  async getUserVideos(token): Promise<VideoEntity[]> {
+    const videos = [];
+    const user = await this.jwtService.verify(token);
+    const userVideos = await this.userVideoRepository.find({ userId: user.id });
+    await Promise.all(
+      userVideos.map(async (video) => {
+        videos.push(await this.getVideoById(video.videoId));
+      }),
+    );
+    return videos;
+  }
+
+  async getVideo(token, videoId): Promise<VideoEntity> {
+    const user = await this.jwtService.verify(token);
+    const userVideos = await this.userVideoRepository.find({ userId: user.id });
+    let video;
+    await Promise.all(
+      userVideos.map(async (userVideo) => {
+        if (userVideo.videoId == videoId) {
+          video = await this.getVideoById(videoId);
+        }
+      }),
+    );
+    if (video) {
+      return video;
+    } else {
+      throw new HttpException('У вас нет такого видео', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async getVideoById(id): Promise<VideoEntity> {
+    return await this.videoRepository.findOne({ id: id });
+  }
+
+  async updateVideo(
+    videoId,
+    videoInput: CreateVideoInput,
+    video: any,
+    token,
+  ): Promise<VideoEntity> {
+    try {
+      const videoEntity = await this.getVideo(token, videoId);
+      console.log(videoEntity);
+      await this.filesService.updateFile(video, videoEntity.video);
+      videoEntity.title = videoInput.title;
+      return await this.videoRepository.save(videoEntity);
+    } catch (e) {
+      throw new HttpException('Ошибка', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async deleteVideo(videoId, token): Promise<string> {
+    const video = await this.getVideo(token, videoId);
+    await this.filesService.deleteVideoFile(video.video);
+    console.log(video);
+    await this.videoRepository.remove(video);
+    const userVideos = await this.userVideoRepository.find({
+      videoId: videoId,
+    });
+    await this.userVideoRepository.remove(userVideos);
+    return 'Удалено';
+  }
+
+  async shareRights(token, videoId, email): Promise<string> {
+    await this.getVideo(token, videoId);
+    const user = await this.userRepository.findOne({ email: email });
+    if (!user) {
+      throw new HttpException(
+        'Пользователя с таким email не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    await this.userVideoRepository.save({ userId: user.id, videoId: videoId });
+    return `Права на видео ${videoId} успешно разделены с пользователем ${user.username}`;
+  }
+}
