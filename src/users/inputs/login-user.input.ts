f359a02cfae51acf268b0445df69a8c46b6c2d77
streamrp75 <steamrp75@gmail.com>
2022-04-14 22:26:53 +0300

    feat: change login input

--- src/users/inputs/login-user.input.ts
+++ src/users/inputs/login-user.input.ts
@@ -1,10 +1,4 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
 export class LoginUserInput {
-  @Field({ nullable: false })
   email: string;
-
-  @Field({ nullable: false })
   password: string;
 }
