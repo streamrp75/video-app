000abf8c5645147cc822f35185083cb6dedc90d0
streamrp75 <steamrp75@gmail.com>
2022-04-25 03:36:45 +0300

    docs: add swagger

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,19 +1,44 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { CreateUserInput } from '../../users/inputs/create-user.input';
 import { AuthService } from '../services/auth.service';
 import { LoginUserInput } from '../../users/inputs/login-user.input';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { VideoEntity } from '../../videos/entities/video.entity';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
-  login(@Body() userDto: LoginUserInput) {
-    return this.authService.login(userDto);
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully logged in',
+    type: [VideoEntity],
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Authorization error',
+  })
+  @ApiBody({ type: LoginUserInput })
+  login(@Body() userDto: LoginUserInput, @Res({ passthrough: true }) response) {
+    return this.authService.login(userDto, response);
   }
-
   @Post('/registration')
-  registration(@Body() userDto: CreateUserInput) {
-    return this.authService.registration(userDto);
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully registered',
+    type: [VideoEntity],
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Authorization error',
+  })
+  @ApiBody({ type: CreateUserInput })
+  registration(
+    @Body() userDto: CreateUserInput,
+    @Res({ passthrough: true }) response,
+  ) {
+    return this.authService.registration(userDto, response);
   }
 }
