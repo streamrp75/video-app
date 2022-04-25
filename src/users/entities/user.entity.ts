c5be1c92aff756fb1f8bfa3a8916498dfc50c1bb
streamrp75 <steamrp75@gmail.com>
2022-04-25 03:26:36 +0300

    docs: add swagger documentation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('users')
 export class UserEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   email: string;
 
+  @ApiProperty()
   @Column()
   username: string;
 
+  @ApiProperty()
   @Column()
   password: string;
 }
--- src/videos/entities/video.entity.ts
+++ src/videos/entities/video.entity.ts
@@ -1,16 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('videos')
 export class VideoEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   title: string;
 
+  @ApiProperty()
   @Column()
   authorId: number;
 
+  @ApiProperty()
   @Column()
   video: string;
 }
