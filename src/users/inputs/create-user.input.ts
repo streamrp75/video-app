bfe1c2f587f2dc031fe3ebb797f3f4ea5761b61e
streamrp75 <steamrp75@gmail.com>
2022-04-25 03:34:39 +0300

    docs: add swagger

--- src/users/inputs/create-user.input.ts
+++ src/users/inputs/create-user.input.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserInput {
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   username: string;
+  @ApiProperty()
   password: string;
 }
--- src/videos/inputs/create-video.input.ts
+++ src/videos/inputs/create-video.input.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateVideoInput {
+  @ApiProperty()
   readonly title: string;
 }
