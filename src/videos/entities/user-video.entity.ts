81f0f29f66b2d89f57b52b1fe4c74175916f59c8
streamrp75 <steamrp75@gmail.com>
2022-04-25 03:29:16 +0300

    docs: add swagger documentation

--- src/videos/entities/user-video.entity.ts
+++ src/videos/entities/user-video.entity.ts
@@ -1,13 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('user-video')
 export class UserVideoEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({ nullable: false })
   userId: number;
 
+  @ApiProperty()
   @Column({ nullable: false })
   videoId: number;
 }
