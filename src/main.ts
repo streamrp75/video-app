fe7164202915817fea4b6fb84e882e02acd2ed7c
streamrp75 <steamrp75@gmail.com>
2022-04-25 03:32:14 +0300

    feat: add swagger feat: add cookie parser for authorization with refresh token

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'reflect-metadata';
 import { ConfigService } from '@nestjs/config';
-
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService);
+  app.use(cookieParser());
+  const svconfig = new DocumentBuilder()
+    .setTitle('Video app')
+    .setDescription('The video API description')
+    .setVersion('1.0')
+    .addTag('video')
+    .build();
+  const document = SwaggerModule.createDocument(app, svconfig);
+  SwaggerModule.setup('swagger', app, document);
   process.env.SECRET_KEY = config.get<string>('SECRET_KEY');
   const port = config.get<number>('API_PORT');
   await app.listen(port || 3000, () => {
